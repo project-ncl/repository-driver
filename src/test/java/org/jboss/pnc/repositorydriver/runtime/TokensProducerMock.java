56ee7f848bc3bebca6ec3c77745a9c5b8d482598
Honza Brázdil <jbrazdil@redhat.com>
2021-10-19 15:22:01 +0200

    [NCL-6783] Use injected @Tokens instead of custom code
    We can use provided quarkus technology instead of trying to write our
    own faulty way to refresh tokens.

---
+++ src/test/java/org/jboss/pnc/repositorydriver/runtime/TokensProducerMock.java
@@ -0,0 +1,23 @@
+package org.jboss.pnc.repositorydriver.runtime;
+
+import java.time.Duration;
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.inject.Produces;
+
+import io.quarkus.oidc.client.Tokens;
+import io.quarkus.test.Mock;
+
+/**
+ * @author <a href="mailto:jbrazdil@gmail.com">Honza Brázdil</a>
+ */
+@Mock
+public class TokensProducerMock {
+
+    @Produces
+    @RequestScoped
+    public Tokens produceTokens() {
+        Tokens tokens = new Tokens("theToken", Long.MAX_VALUE, Duration.ofDays(1), "refreshToken", null);
+
+        return tokens;
+    }
+}
