ec6d7862d066eb3e41dc2991b26d0d0433bb7be4
Honza Brázdil <jbrazdil@redhat.com>
2024-03-14 18:26:28 +0100

    [NCL-8471] Upload logs to bifrost

---
+++ src/main/java/org/jboss/pnc/repositorydriver/runtime/BifrostLogUploaderProducer.java
@@ -0,0 +1,54 @@
+/**
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2021 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.pnc.repositorydriver.runtime;
+
+import io.quarkus.oidc.client.OidcClient;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.jboss.pnc.bifrost.upload.BifrostLogUploader;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
+import java.net.URI;
+
+@ApplicationScoped
+public class BifrostLogUploaderProducer {
+
+    @Inject
+    OidcClient oidcClient;
+    private final BifrostLogUploader logUploader;
+
+    public BifrostLogUploaderProducer(
+            @ConfigProperty(name = "repository-driver.bifrost-uploader.api-url") URI bifrostUrl,
+            @ConfigProperty(name = "repository-driver.bifrost-uploader.maxRetries", defaultValue = "6") int maxRetries,
+            @ConfigProperty(
+                    name = "repository-driver.bifrost-uploader.retryDelay",
+                    defaultValue = "10") int retryDelay) {
+        logUploader = new BifrostLogUploader(bifrostUrl, maxRetries, retryDelay, this::getFreshAccessToken);
+    }
+
+    private String getFreshAccessToken() {
+        return oidcClient.getTokens().await().indefinitely().getAccessToken();
+    }
+
+    @Produces
+    public BifrostLogUploader produce() {
+        return logUploader;
+    }
+}
--- src/test/java/org/jboss/pnc/repositorydriver/WithSidecarTest.java
+++ src/test/java/org/jboss/pnc/repositorydriver/WithSidecarTest.java
@@ -1,15 +1,24 @@
 package org.jboss.pnc.repositorydriver;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.quarkus.test.junit.QuarkusMock;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.junit.TestProfile;
 import io.quarkus.test.security.TestSecurity;
+import io.restassured.RestAssured;
 import org.jboss.pnc.api.enums.BuildType;
 import org.jboss.pnc.api.repositorydriver.dto.RepositoryCreateRequest;
 import org.jboss.pnc.api.repositorydriver.dto.RepositoryCreateResponse;
+import org.jboss.pnc.bifrost.upload.BifrostLogUploader;
+import org.jboss.pnc.repositorydriver.invokerserver.CallbackHandler;
+import org.jboss.pnc.repositorydriver.invokerserver.HttpServer;
+import org.jboss.pnc.repositorydriver.invokerserver.ServletInstanceFactory;
 import org.jboss.pnc.repositorydriver.profile.WithSidecar;
+import org.jboss.pnc.repositorydriver.runtime.BifrostLogUploaderProducer;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import javax.inject.Inject;
 import javax.ws.rs.core.MediaType;
@@ -26,6 +35,15 @@ public class WithSidecarTest {
 
     @Inject
     Configuration configuration;
+
+    @BeforeAll
+    public static void beforeClass() throws Exception {
+        BifrostLogUploader bifrostLogUploader = Mockito.mock(BifrostLogUploader.class);
+        Mockito.doNothing().when(bifrostLogUploader).uploadString(Mockito.any(), Mockito.any());
+        BifrostLogUploaderProducer bifrostLogUploaderProducer = Mockito.mock(BifrostLogUploaderProducer.class);
+        Mockito.when(bifrostLogUploaderProducer.produce()).thenReturn(bifrostLogUploader);
+        QuarkusMock.installMockForType(bifrostLogUploaderProducer, BifrostLogUploaderProducer.class);
+    }
 
     @Test
     public void testWithSidecarEnabled() {
