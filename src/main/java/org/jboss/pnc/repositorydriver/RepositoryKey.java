aad77e943254c701d6ff66809fa00602eeba642e
Nick Cross <ncross@redhat.com>
2026-06-05 10:28:49 +0100

    Fix generic repo promotion and tests

--- src/main/java/org/jboss/pnc/repositorydriver/RepositoryKey.java
+++ src/main/java/org/jboss/pnc/repositorydriver/RepositoryKey.java
@@ -8,6 +8,6 @@ import org.jboss.pnc.api.tracker.dto.PackageType;
  * about a repository
  */
 public record RepositoryKey(RepositoryId repositoryId,
-                            PackageType packageType,
-                            boolean temporary) {
+        PackageType packageType,
+        boolean temporary) {
 }
